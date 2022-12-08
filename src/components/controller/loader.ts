5eb15163348e422794f42238d46bbb61c415fe6b
xDamneDx <xdamnedx@gmail.com>
2022-12-08 21:19:21 +0200

    refactor: migrate loader to ts

---
+++ src/components/controller/loader.ts
@@ -0,0 +1,66 @@
+import { Callback } from '../../utils/types';
+
+enum Status {
+    UNAUTHORIZED = 401,
+    NOTFOUND = 404,
+}
+
+interface Options {
+    apiKey?: string;
+    sources?: string;
+}
+
+interface GetResponse {
+    endpoint: string;
+    options?: Options;
+}
+
+class Loader {
+    baseLink: string;
+    options: Options;
+
+    constructor(baseLink: string, options: Options) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp<T>(
+        { endpoint, options = {} }: GetResponse,
+        callback: Callback<T> = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === Status.UNAUTHORIZED || res.status === Status.NOTFOUND)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: object, endpoint: string) {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        (Object.keys(urlOptions) as Array<keyof typeof urlOptions>).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load<T>(method: 'POST' | 'GET', endpoint: string, callback: Callback<T>, options: Options = {}) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
